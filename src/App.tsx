ebfb32a59c6891318aec1e42d8348c00db8fba85
Reza Roustaei <reza@edurino.com>
2024-07-30 01:10:51 +0200

    Revert "change app"
    This reverts commit 282c0b0f8164f60d8fa83324fa865f7db3fc1d52.

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+import WebApp from '@twa-dev/sdk'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>سلام خره!</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          من خرم {count}
+        </button>
+      </div>
+      <div className='card'>
+        <button onClick={() => WebApp.showAlert(`${count}  : میزان خریت من `)}>
+          Show Alert...
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
