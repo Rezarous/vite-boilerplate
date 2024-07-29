e4837f4d687d87a7c91a3ffa1562055731dca490
Reza Roustaei <reza@edurino.com>
2024-07-29 14:15:17 +0200

    farsi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import WebApp from '@twa-dev/sdk'
@@ -11,29 +8,17 @@ function App() {
 
   return (
     <>
-      <div>سلام خره!</div>
-      {/* <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>TWA + Vite + React</h1>
-      <div className="card">
+      <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          من خرم {count}
         </button>
       </div>
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
+      <div className='card'>
+        <button onClick={() => WebApp.showAlert(`${count}  : میزان خریت من `)}>
+          Show Alert...
         </button>
-      </div> */}
+      </div>
     </>
   )
 }
